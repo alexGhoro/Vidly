e395f8d7c4741e8b4ff0212d26928456a213dcb3
Alejandro Matute <alexghoro@outlook.com>
2017-07-02 02:29:33 -0400

    Add Edit Customer feautre

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -33,13 +33,48 @@ namespace Vidly.Controllers
     public ActionResult New()
     {
       var membershipTypes = _context.MembershipTypes.ToList();
-      var viewModel = new NewCustomerViewModel
+      var viewModel = new CustomerFormViewModel
       {
         MembershipTypes = membershipTypes
       };
-      return View(viewModel);
+      return View("CustomerForm", viewModel);
     }
- 
+
+    [HttpPost]
+    public ActionResult Save(Customer customer)
+    {
+      if (customer.Id == 0)
+      {
+        _context.Customers.Add(customer);
+      }
+      else
+      {
+        var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+        customerInDb.Name = customer.Name;
+        customerInDb.Birthdate = customer.Birthdate;
+        customerInDb.MembershipType = customer.MembershipType;
+        customerInDb.SubscribedToNewsletter = customer.SubscribedToNewsletter;
+      }
+      _context.SaveChanges();
+
+      return RedirectToAction("Index", "Customers");
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+      if (customer == null)
+        return HttpNotFound();
+
+      var viewModel = new CustomerFormViewModel
+      {
+        Customer = customer,
+        MembershipTypes = _context.MembershipTypes.ToList()
+      };
+      return View("CustomerForm",viewModel);
+    }
+     
     public ActionResult Details(int id)
     {
       var customer = _context.Customers.Include(m => m.MembershipType).SingleOrDefault(c => c.Id == id);
